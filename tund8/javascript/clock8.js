23346a65cfeca72b56bed890efc0ffa4ad83dad4
Marilii Saar <mariliisaar@users.noreply.github.com>
2021-04-05 20:43:33 +0300

    Clock tolls on the full hour. Alarm clock functionality

--- tund8/javascript/clock8.js
+++ tund8/javascript/clock8.js
@@ -3,9 +3,13 @@ let clockSpeaker = new Audio();
 let timeWords = [];
 let bell = new Audio();
 let prevHour;
+let animationID;
+let repeat;
 
 function initClock() {
     document.getElementById("clock_speak_btn").addEventListener("click", tellTime);
+    // Kas äratuskell on seatud?
+    document.getElementById("set_alarm_btn").addEventListener("change", setAlarm);
     bell.src = soundUrl + "kell.mp3";
     prevHour = new Date().getHours();
     clockTick();
@@ -23,14 +27,72 @@ function clockTick() {
     document.getElementById("minutehand").style.transform = "rotate(" + minAngle +"deg)";
     document.getElementById("hourhand").style.transform = "rotate(" + hourAngle +"deg)";
     // Kas lüüa kella?
-    if (currentMinutes == 22 && currentSeconds == 0 && currentTime.getMilliseconds() < 1000 / 60 && document.getElementById("allow_bell_btn").checked) { // <-- Testimiseks
-        // Kavalam on kontrollida, if (document.getElementById("allow_bell_btn").checked && currentHour != prevHour) {
+    // if (currentMinutes == 0 && currentSeconds == 0 && currentTime.getMilliseconds() < 1000 / 60 && document.getElementById("allow_bell_btn").checked) { // <-- Testimiseks
+    if (document.getElementById("allow_bell_btn").checked && currentHour != prevHour) {
         // loendur, mitu korda vaja lüüa
+        let tollCount = 0;
+        let count = 0;
+        if(currentHour == 0) {
+            tollCount = 12;
+        } else if (currentHour > 0 && currentHour < 13) {
+            tollCount = currentHour;
+        } else {
+            tollCount = currentHour - 12;
+        }
+
         bell.play();
+        count++;
+        bell.onended = function() {
+            if (count < tollCount) {
+                bell.play();
+                count++;
+            }
+        }
+
         prevHour = currentHour;
     }
     // setTimeout(1000, clockTick); <-- iga sekundi järel (sekund on ebasobimatu ajaühik, setTimeout ei saa hakkama (aeg-ajalt läheb +1 millisekund))
     requestAnimationFrame(clockTick); // teist akent vaadates jääb seisma
+}
+
+function setAlarm() {
+    // console.log("Set");
+    let currentTime = new Date();
+    let alarm = new Audio();
+    alarm.src = "../media/moo.mp3";
+    alarm.volume = 0.1;
+    if (document.getElementById("set_alarm_btn").checked) {
+        document.getElementById("alarmLabel").innerHTML = "Lülita äratuskell välja";
+        repeat = true;
+    } else {
+        document.getElementById("alarmLabel").innerHTML = "Lülita äratuskell sisse";
+        if (animationID != null) {
+            cancelAnimationFrame(animationID);
+            animationId = null;
+        }
+        repeat = false;
+    }
+    
+    if (document.getElementById("alarm_hour_btn").value == currentTime.getHours() && document.getElementById("alarm_minute_btn").value == currentTime.getMinutes() && currentTime.getSeconds() == 0) {
+        cancelAnimationFrame(animationID);       
+        ringAlarm(alarm);
+    } else if (!document.getElementById("set_alarm_btn").checked) {
+        cancelAnimationFrame(setAlarm);
+    } else {
+        animationID = requestAnimationFrame(setAlarm);
+    }
+}
+
+function ringAlarm(alarm) {
+    alarm.play();
+    alarm.onended = function() {
+        if (repeat) {
+            if (alarm.volume <= 0.9) {
+                alarm.volume += 0.1;
+            }
+            alarm.play();
+        }
+    }  
 }
 
 function tellTime() {
